3b17511b45c3c8983b4f48360b25b96644fbbdc2
Animesh Manglik <amanglik@whalerockindustries.com>
2016-12-12 11:22:48 -0800

    Swift Development: Egg Timer App

--- Egg Timer/ViewController.swift
+++ Egg Timer/ViewController.swift
@@ -10,6 +10,48 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var timer = Timer()
+    var time = 200
+    
+    @IBOutlet weak var timerValue: UILabel!
+   
+    func processTimer() {
+        
+        if (time > 0) {
+            time -= 1
+            timerValue.text = String(time)
+        } else {
+            timer.invalidate()
+        }
+
+    }
+
+    @IBAction func timerPausePressed(_ sender: AnyObject) {
+        timer.invalidate()
+    }
+
+    @IBAction func timerPlayPressed(_ sender: AnyObject) {
+        
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(ViewController.processTimer), userInfo: nil, repeats: true)
+    }
+
+    @IBAction func timerAddPressed(_ sender: AnyObject) {
+        time += 10
+        timerValue.text = String(time)
+    }
+    
+    @IBAction func timerReducePressed(_ sender: AnyObject) {
+        if (time > 10) {
+            time -= 10
+            timerValue.text = String(time)
+        }
+
+    }
+    
+    @IBAction func timerResetPressed(_ sender: AnyObject) {
+        time = 200
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
---
+++ README.md
@@ -0,0 +1,17 @@
+#Swift Development
+
+## Egg Timer
+
+Using concepts of
+
+- Tables
+- Permanent Storage
+- 2 view controllers
+
+Egg Timer is a timer that starts from 200 -> 0, you can
+
+- Start
+- Pause
+- Reset
+- Add 10 Seconds
+- Reduce 10 Seconds
